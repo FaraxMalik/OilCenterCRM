79c163b012a31cf743ad639421f1d38b4311475f
Abdul Ahad <162739792+mabdulahadch@users.noreply.github.com>
2024-10-25 09:56:43 +0500

    Main function with DB connection

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello Bois");
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        String testQuery = "select * from Customer";
+
+        try (Connection con = DBConnection.getConnection();
+        PreparedStatement preparedStatement = con.prepareStatement(testQuery)){
+
+            ResultSet res = preparedStatement.executeQuery();
+
+            while (res.next()) {
+                int id = res.getInt("customerID");
+                String name = res.getString("CustomerName");
+                String phone = res.getString("PhoneNumber");
+                String address= res.getString("Address");
+
+                System.out.println(id+","+name+","+phone+","+address);
+            }
+
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
---
+++ src/DBConnection.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:mysql://crm.cj8wgw6c6hal.ap-south-1.rds.amazonaws.com:3306/CRM?zeroDateTimeBehavior=CONVERT_TO_NULL&encrypt=true&trustServerCertificate=true";
+    private static final String USER = "admin";
+    private static final String PASSWORD = "awscrmscd";
+
+    public static Connection getConnection() throws ClassNotFoundException {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("DB connection successful!");
+        } catch (SQLException e) {
+            System.out.println("DB connection failed: " + e.getMessage());
+        }
+        return conn;
+    }
+}
