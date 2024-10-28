a661521b7dc5f968114c63acc36755f621f7225c
Hamza Naveed <hamzanaveed981@gmail.com>
2024-10-29 00:06:56 +0500

    separated the DB file

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-
+import DatabaseConnection.*;
 public class App {
 
     public static void main(String[] args) throws Exception {
